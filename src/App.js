2d77be0e3a3577cf1313675b73fe5766d4ed14a1
egesua <egesu06@gmail.com>
2024-03-14 18:59:18 +0300

    app.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>
-        
+      <div className='bg-blue-500 h-4'>
+        Welcome
       </div>
     </div>
   );
