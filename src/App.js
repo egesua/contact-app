2b69c2a00b27aad57e0da7ce0b5bf888c96ea9f2
egesua <egesu06@gmail.com>
2024-03-17 18:09:07 +0300

    some css improvements.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className='bg-blue-500 h-4'>
-        Welcome
-      </div>
+    <div>
+      <h1>
+        Hello
+      </h1>
     </div>
   );
 }
