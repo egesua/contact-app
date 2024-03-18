68b6e3cd3a165f4d837eee8239b69a3b11f5ae62
egesua <egesu06@gmail.com>
2024-03-18 23:22:05 +0300

    fetching contacts from our db

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { getContacts } from "./api/ContactService";
 
 function App() {
+  const [data, setData] = useState({});
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const getAllContacts = async (page = 0, size = 10) => {
+    try {
+      setCurrentPage(page);
+      const { data } = await getContacts(page, size);
+      setData(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllContacts();
+  }, []);
+
   return (
     <div>
-      <h1>
-        Hello
-      </h1>
+      <h1>Hello</h1>
     </div>
   );
 }
