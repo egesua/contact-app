c569128adf8b1f9710444b9eba243ed512759245
egesua <egesu06@gmail.com>
2024-03-18 22:46:14 +0300

    previous page button created

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,14 +1,33 @@
-import React from "react"
+import React from "react";
 import Contact from "./Contact";
 
 const ContactList = ({ data, currentPage, getAllContacts }) => {
   return (
     <main className="main">
-      {data?.content?.length === 0 && <div>No Contacts. Please add a new contact.</div>}
+      {data?.content?.length === 0 && (
+        <div>No Contacts. Please add a new contact.</div>
+      )}
 
-      {data?.content?.length > 0 && data.content.map(contact => <Contact contact={contact} key={contact.id} />)}
+      <ul className="contact__list">
+        {data?.content?.length > 0 &&
+          data.content.map((contact) => (
+            <Contact contact={contact} key={contact.id} />
+          ))}
+      </ul>
+
+      {data?.content?.length > 0 && data?.totalPages > 1 && (
+        <div className="pagination">
+          <a
+            onClick={() => getAllContacts(currentPage - 1)}
+            className={0 === currentPage ? "disabled" : ""}
+          >
+            &laquo;
+          </a>
+          <a></a>
+        </div>
+      )}
     </main>
-  )
+  );
 };
 
 export default ContactList;
