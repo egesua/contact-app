223393b5ffb9176b362f2dd2a3b1ddb476ccf790
egesua <egesu06@gmail.com>
2024-03-19 13:02:38 +0300

    debugging and unused import cleaning

--- src/api/ContactService.js
+++ src/api/ContactService.js
@@ -1,22 +1,27 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8585/contacts";
+const API_URL = 'http://localhost:8585/contacts';
 
 export async function saveContact(contact) {
     return await axios.post(API_URL, contact);
 }
+
 export async function getContacts(page = 0, size = 10) {
-    return await axios.get(`${API_URL}?page=${page}$size=${size}`);
+    return await axios.get(`${API_URL}?page=${page}&size=${size}`);
 }
+
 export async function getContact(id) {
-    return await axios.post(`${API_URL}/${id}`);
+    return await axios.get(`${API_URL}/${id}`);
 }
-export async function updateContact(contact) {
+
+export async function udpateContact(contact) {
     return await axios.post(API_URL, contact);
 }
-export async function updatePhoto(formData) {
+
+export async function udpatePhoto(formData) {
     return await axios.put(`${API_URL}/photo`, formData);
 }
+
 export async function deleteContact(id) {
     return await axios.delete(`${API_URL}/${id}`);
 }
